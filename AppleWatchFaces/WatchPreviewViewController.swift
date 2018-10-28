d25f70a55b0edea5473122a6b70bd53885343a1f
Michael Hill <michaelhill@michaels-mbp.home>
2018-10-28 01:46:04 -0400

    running shared preview

--- AppleWatchFaces/WatchPreviewViewController.swift
+++ AppleWatchFaces/WatchPreviewViewController.swift
@@ -11,9 +11,26 @@ import SpriteKit
 
 class WatchPreviewViewController: UIViewController {
 
+    @IBOutlet var skView: SKView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Load the SKScene
+        if let scene = SKWatchScene(fileNamed: "SKWatchScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            skView.presentScene(scene)
+        }
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
     }
 
 }
