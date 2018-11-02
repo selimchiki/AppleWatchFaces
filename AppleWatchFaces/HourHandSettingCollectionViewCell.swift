405387656fe6049eb16dc7ffad4860fc2ed06b70
Michael Hill <michaelhill@Michaels-MBP.home>
2018-11-02 17:19:06 -0400

    added minute hand selectors

---
+++ AppleWatchFaces/HourHandSettingCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SecondHandSettingCollectionViewCell.swift
+//  AppleWatchFaces
+//
+//  Created by Michael Hill on 10/29/18.
+//  Copyright © 2018 Michael Hill. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MinuteHandSettingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var skView : SKView!
+    var minuteHandType: MinuteHandTypes = MinuteHandTypes.MinuteHandTypeSwiss
+    
+}
