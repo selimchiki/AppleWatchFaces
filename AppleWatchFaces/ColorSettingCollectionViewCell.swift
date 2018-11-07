dccadfada4befe23fc081bd30bf0e8a76b0570cd
Michael Hill <michaelhill@michaels-mbp.home>
2018-11-06 20:41:49 -0500

    colors have circle with border

--- AppleWatchFaces/ColorSettingCollectionViewCell.swift
+++ AppleWatchFaces/ColorSettingCollectionViewCell.swift
@@ -7,56 +7,59 @@
 //
 
 import UIKit
+import SpriteKit
 
-@IBDesignable
+//@IBDesignable
 class ColorSettingCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var circleView: UIView!
     
-    @IBInspectable var borderWidth: CGFloat = 0 {
-        didSet {
-            layer.borderWidth = borderWidth
-        }
-    }
-    //Normal state bg and border
-    @IBInspectable var normalBorderColor: UIColor? {
-        didSet {
-            layer.borderColor = normalBorderColor?.cgColor
-        }
-    }
-    
-    @IBInspectable var normalBackgroundColor: UIColor? {
-        didSet {
-            setBgColorForState(color: normalBackgroundColor, forState: .normal)
-        }
-    }
-    
-    
-    //Highlighted state bg and border
-    @IBInspectable var highlightedBorderColor: UIColor?
-    
-    @IBInspectable var highlightedBackgroundColor: UIColor? {
-        didSet {
-            setBgColorForState(color: highlightedBackgroundColor, forState: .highlighted)
-        }
-    }
-    
-    
-    private func setBgColorForState(color: UIColor?, forState: UIControl.State){
-        if color != nil {
-            self.layer.backgroundColor = color?.cgColor
-        }
-    }
+//    @IBInspectable var borderWidth: CGFloat = 0 {
+//        didSet {
+//            layer.borderWidth = borderWidth
+//        }
+//    }
+//    //Normal state bg and border
+//    @IBInspectable var normalBorderColor: UIColor? {
+//        didSet {
+//            layer.borderColor = normalBorderColor?.cgColor
+//        }
+//    }
+//
+//    @IBInspectable var normalBackgroundColor: UIColor? {
+//        didSet {
+//            setBgColorForState(color: normalBackgroundColor, forState: .normal)
+//        }
+//    }
+//
+//
+//    //Highlighted state bg and border
+//    @IBInspectable var highlightedBorderColor: UIColor?
+//
+//    @IBInspectable var highlightedBackgroundColor: UIColor? {
+//        didSet {
+//            setBgColorForState(color: highlightedBackgroundColor, forState: .highlighted)
+//        }
+//    }
+//
+//
+//    private func setBgColorForState(color: UIColor?, forState: UIControl.State){
+//        if color != nil {
+//            self.layer.backgroundColor = color?.cgColor
+//        }
+//    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        layer.cornerRadius = layer.frame.height / 2
-        //self.titleEdgeInsets = UIEdgeInsets.init(top: 2, left: 5, bottom: 2, right: 5)
+        circleView.layer.cornerRadius = circleView.layer.frame.height / 2
+        circleView.layer.borderWidth = 2.0
+        circleView.layer.borderColor = SKColor.white.cgColor
+        
         clipsToBounds = true
         
-        if borderWidth > 0 {
-                layer.borderColor = normalBorderColor?.cgColor
-        }
+//        if borderWidth > 0 {
+//                layer.borderColor = normalBorderColor?.cgColor
+//        }
     }
 }
