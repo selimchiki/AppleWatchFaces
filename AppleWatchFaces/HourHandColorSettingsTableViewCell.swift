1a1ce1c4a4597e5a73d230c74478826614f52252
Michael Hill <michaelhill@michaels-macbook-pro.local>
2018-11-11 23:27:35 -0500

    hour and minute hand selection now

--- AppleWatchFaces/HourHandColorSettingsTableViewCell.swift
+++ AppleWatchFaces/HourHandColorSettingsTableViewCell.swift
@@ -12,6 +12,23 @@ import SpriteKit
 
 class HourHandColorSettingsTableViewCell: ColorSettingsTableViewCell {
     
+    @IBOutlet var hourHandColorSelectionCollectionView: UICollectionView!
+    
+    // called after a new setting should be selected ( IE a new design is loaded )
+    override func chooseSetting( animated: Bool ) {
+        //debugPrint("** SecondHandColorSettingsTableViewCell called **" + SettingsViewController.currentClockSetting.clockFaceSettings!.hourHandMaterialName)
+        
+        if let clockFaceSettings = SettingsViewController.currentClockSetting.clockFaceSettings {
+            let filteredColor = colorListVersion(unfilteredColor: clockFaceSettings.hourHandMaterialName)
+            if let materialColorIndex = colorList.firstIndex(of: filteredColor) {
+                let indexPath = IndexPath.init(row: materialColorIndex, section: 0)
+                
+                //scroll and set native selection
+                hourHandColorSelectionCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: UICollectionView.ScrollPosition.right)
+            }
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let newColor = colorList[indexPath.row]
         debugPrint("selected cell hourHandColor: " + newColor)
--- AppleWatchFaces/MinuteHandColorSettingsTableViewCell.swift
+++ AppleWatchFaces/MinuteHandColorSettingsTableViewCell.swift
@@ -11,6 +11,23 @@ import SpriteKit
 
 class MinuteHandColorSettingsTableViewCell: ColorSettingsTableViewCell {
     
+    @IBOutlet var minuteHandColorSelectionCollectionView: UICollectionView!
+    
+    // called after a new setting should be selected ( IE a new design is loaded )
+    override func chooseSetting( animated: Bool ) {
+        //debugPrint("** SecondHandColorSettingsTableViewCell called **" + SettingsViewController.currentClockSetting.clockFaceSettings!.minuteHandMaterialName)
+        
+        if let clockFaceSettings = SettingsViewController.currentClockSetting.clockFaceSettings {
+            let filteredColor = colorListVersion(unfilteredColor: clockFaceSettings.minuteHandMaterialName)
+            if let materialColorIndex = colorList.firstIndex(of: filteredColor) {
+                let indexPath = IndexPath.init(row: materialColorIndex, section: 0)
+                
+                //scroll and set native selection
+                minuteHandColorSelectionCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: UICollectionView.ScrollPosition.right)
+            }
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let newColor = colorList[indexPath.row]
         debugPrint("selected cell minuiteHandColor: " + newColor)
